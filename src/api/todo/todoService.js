abe1f3385bc32de98d0244ece19f13e83ef02a99
JordanaN <jordanasnogueira@gmail.com>
2018-08-27 09:38:57 -0300

    first commit backend app

---
+++ src/api/todo/todoService.js
@@ -0,0 +1,6 @@
+const Todo = require('./todo')
+
+Todo.methods(['get', 'post', 'put', 'delete'])
+Todo.updateOptions({ new: true, runValidators: true }) //retorna o objeto atualizado e habilita as validações de required
+
+module.exports = Todo
