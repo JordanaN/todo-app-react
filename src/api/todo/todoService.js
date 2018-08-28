9cf3991377ef4d129b18f10cab8a8a8f50a8c235
JordanaN <jordanasnogueira@gmail.com>
2018-08-28 10:36:12 -0300

    finalização do backend todo-app

--- src/api/todo/todoService.js
+++ src/api/todo/todoService.js
@@ -1,6 +1,6 @@
 const Todo = require('./todo')
 
 Todo.methods(['get', 'post', 'put', 'delete'])
-Todo.updateOptions({ new: true, runValidators: true }) //retorna o objeto atualizado e habilita as validações de required
+Todo.updateOptions({new: true, runValidators: true}) //retorna o objeto atualizado e habilita as validações de required
 
 module.exports = Todo
--- src/config/routes.js
+++ src/config/routes.js
@@ -1,1 +1,12 @@
 const express = require('express')
+
+module.exports = function(server) {
+
+  //Api Routes
+  const router = express.Router()
+  server.use('/api', router) //middleware para rotas começarem com /api
+
+  //Todo Routes
+  const todoService = require('../api/todo/todoService')
+  todoService.register(router, '/todos') // registrando os metódos da api setados no arquivo todoService
+}
